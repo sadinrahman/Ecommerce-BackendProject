67a07c53955030b2d9a66781cccd4e833e0c7a30
sadinrahman <sadinsadinrahman@gmail.com>
2024-12-19 17:26:23 +0530

    just

--- Models/CartItems.cs
+++ Models/CartItems.cs
@@ -1,18 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
 
 namespace BackendProject.Models
 {
 	public class CartItems
 	{
-		public int Id { get; set; }
-		[Required]
-		public int CartId {  get; set; }
-		[Required]
-		public int ProductId { get; set; }
-		[Required]
-		public int Quantity { get; set; }
-		public virtual Cart? Cart { get; set; }
-		public virtual Product? Product { get; set; }
+
+		public class OrderItems
+		{
+			public int Id { get; set; } 
+			public int OrderId { get; set; } 
+			public int ProductId { get; set; } 
+			public int Quantity { get; set; }
+
+			public virtual Order? Order { get; set; }
+			public virtual Product? Product { get; set; }
+		}
+
 
 	}
 }
